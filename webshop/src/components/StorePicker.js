7a95c2c7a470b3132fc08504ba06903586e6b4ce
Gabi <deutschgabor92@gmail.com>
2020-07-22 23:45:19 +0200

    Event, Ref and this Bind

--- webshop/src/components/StorePicker.js
+++ webshop/src/components/StorePicker.js
@@ -1,14 +1,30 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    //1. Stop the form from submitting
+    event.preventDefault();
+    //2. get the text from that input   Don't Touch the DOM!!!!
+    console.log(this);
+    //3. change the page to /store/whatever-thex-entered
+  };
   render() {
     return (
       //hasznos alkalmazni a React.Fragment tag-et ha több HTML elemet akarok kiiratni a DOM-ba!!!
       <React.Fragment>
-        <form action="" className="store-selector">
-                <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" />
-                <button type="submit">Visit Store </button>
+        <form action="" className="store-selector" onSubmit={this.goToStore}>
+          <h2>Please Enter A Store</h2>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
+          <button type="submit">Visit Store </button>
         </form>
       </React.Fragment>
     );
