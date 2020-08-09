a61f9761394756d86243e3becb3ddc6a611d6958
Gabi <deutschgabor92@gmail.com>
2020-08-09 22:49:53 +0200

    Component Validation with PropTypes

--- webshop/src/components/Inventory.js
+++ webshop/src/components/Inventory.js
@@ -1,13 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
 
 class Inventory extends React.Component {
+  static propTypes = {
+    fishes: PropTypes.object,
+    updateFish: PropTypes.func,
+    deleteFish: PropTypes.func,
+    loadSampleFishes: PropTypes.func,
+    addFish: PropTypes.func,
+  };
   render() {
     return (
       <div className="inventory">
         <h2>Inventory</h2>
-        {Object.keys(this.props.fishes).map(key => (
+        {Object.keys(this.props.fishes).map((key) => (
           <EditFishForm
             key={key}
             index={key}
--- webshop/src/components/StorePicker.js
+++ webshop/src/components/StorePicker.js
@@ -1,34 +1,36 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
   myInput = React.createRef();
+  static propTypes = {
+    history: PropTypes.object,
+  };
 
   goToStore = (event) => {
-    //1. Stop the form from submitting
+    // 1. Stop the form from submitting
     event.preventDefault();
-    //2. get the text from that input   Don't Touch the DOM!!!!
-    const storeName = this.myInput.value;
-    //3. change the page to /store/whatever-thex-entered
+    // 2. get the text from that input
+    const storeName = this.myInput.current.value;
+    // 3. Change the page to /store/whatever-they-entered
     this.props.history.push(`/store/${storeName}`);
   };
   render() {
     return (
-      //hasznos alkalmazni a React.Fragment tag-et ha több HTML elemet akarok kiiratni a DOM-ba!!!
-      <React.Fragment>
-        <form action="" className="store-selector" onSubmit={this.goToStore}>
-          <h2>Please Enter A Store</h2>
-          <input
-            type="text"
-            ref={this.myInput}
-            required
-            placeholder="Store Name"
-            defaultValue={getFunName()}
-          />
-          <button type="submit">Visit Store </button>
-        </form>
-      </React.Fragment>
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store →</button>
+      </form>
     );
   }
 }
+
 export default StorePicker;
